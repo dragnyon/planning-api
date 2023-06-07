a9412b4dd5d6d7c06456a230a79ba93cf2944564
pc colin fix <alves.colin@icloud.com>
2023-06-07 20:59:05 +0200

    Delete react

--- planning/planning.WebApplication/Program.cs
+++ planning/planning.WebApplication/Program.cs
@@ -36,10 +36,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
-
-app.MapFallbackToFile("index.html");
 
 app.Run();
